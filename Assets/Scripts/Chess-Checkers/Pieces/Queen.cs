582304b61412b821bbc21c9c4073190a3dbbfc2c
alexp230 <apruitt230@gmail.com>
2024-11-10 21:27:58 -0600

    Added ChessPieces prefabs

---
+++ Assets/Scripts/Chess-Checkers/Pieces/Queen.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+public class Queen : GenericPiece
+{
+    public override void InstantiatePieceComponents(bool forP1)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override List<int> GetValidMoves(GenericPiece currentPiece, bool getOnlyJumps = false)
+    {
+        throw new System.NotImplementedException();
+    }
+}
