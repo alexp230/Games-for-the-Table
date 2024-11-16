9a4758fbde9a450d883bbe6f509ab7c768ea064e
alexp230 <apruitt230@gmail.com>
2024-11-15 18:12:42 -0600

    Tile Can now detect is enpassant is capturable tile

--- Assets/Scripts/Chess-Checkers/Tile.cs
+++ Assets/Scripts/Chess-Checkers/Tile.cs
@@ -19,18 +19,31 @@ public class Tile : MonoBehaviour
         TilePositions.Add(ChessBoard.PosToBoardPos(pos), this);
     }
 
-    public static void HighlightTiles(List<int> positions)
+    public static void HighlightTiles(GenericPiece currentPiece)
     {
+        List<int> positions = currentPiece.ValidMoves;
         GenericPiece[] board = ChessBoard.Board;
         foreach (int pos in positions)
         {
             GameObject highlight = TilePositions[pos].HighLight;
             
-            highlight.transform.localScale = board[pos] ? new Vector3(1, 0.1f, 1) : new Vector3(0.25f, 0.1f, 0.25f);
+            highlight.transform.localScale = CaptureTile(pos) ? new Vector3(1, 0.1f, 1) : new Vector3(0.25f, 0.1f, 0.25f);
 
             highlight.SetActive(true);
             HighLightedPositions.Add(pos);
         }
+
+        bool CaptureTile(int position)
+        {
+            if (currentPiece is Pawn pawn)
+            {
+                if (pawn.CanEnPassantLeft)
+                    return GenericPiece.IsP1Piece(pawn) ? board[position-9] : board[position+7];
+                else if (pawn.CanEnPassantRight)
+                    return GenericPiece.IsP1Piece(pawn) ? board[position-7] : board[position+9];
+            }
+            return board[position];
+        }   
     }
 
     public static void DeHighLightTiles()
