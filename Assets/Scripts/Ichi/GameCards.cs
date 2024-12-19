e9dfdf5df8954689e36cac3c8dcfd841e9d94d55
alexp230 <apruitt230@gmail.com>
2024-12-19 16:36:53 -0600

    Started works on new game

---
+++ Assets/Scripts/Ichi/GameCards.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GameCards
+{
+    /*
+    Reverse: reverse the current turn order
+
+    Skip: skips the turn of the next player
+
+    Choice: change the color of the stack 
+    +2 Choice | +4 Choice | +8 Choice
+
+    Choice-Erase: everyone discard all the cards of the chosen color from their deck's
+
+    Shift: shifts each deck 'x' times of the turn order
+
+    Time-bomb: must make a move in three seconds, or else you draw cards
+
+
+    Abilities:
+    Jump-in: player can play out of turn in the played card is the same color and kind
+
+    Reveal: player reveals everyone's cards temporarily
+
+    Fortune-Teller: can see the next five draw cards
+
+        
+    */
+    
+}
