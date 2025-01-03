ddf35baf3efbd9c469b63cd54958f0652e5600e5
alexp230 <apruitt230@gmail.com>
2025-01-03 15:04:14 -0600

    Cleaned Up code

--- Assets/Scripts/Ichi/Card.cs
+++ Assets/Scripts/Ichi/Card.cs
@@ -3,6 +3,8 @@ using TMPro;
 
 public class Card : MonoBehaviour
 {
+    private const int CARD_LIFT = 20;
+
     [SerializeField] private CardSO Card_SO;
 
     [SerializeField] private TextMeshProUGUI MiddleText;
@@ -11,13 +13,6 @@ public class Card : MonoBehaviour
     [SerializeField] private BoxCollider BoxCollider;
 
     private char Value;
-    private const int CARD_LIFT = 20;
-
-    void Start()
-    {
-        // this.SetColor();
-        // this.SetValue();
-    }
 
     void OnMouseEnter()
     {
@@ -40,38 +35,25 @@ public class Card : MonoBehaviour
     
     public void SetColor(string color = "")
     {
-        if (color == "")
-        {
-            Material[] mats = new Material[4] {Card_SO.ColorRed, Card_SO.ColorYellow, Card_SO.ColorGreen, Card_SO.ColorCyan};
-            this.GetComponent<MeshRenderer>().material = mats[Random.Range(0, mats.Length)];
-            return;
-        }
+        Material[] mats = new Material[4] {Card_SO.ColorRed, Card_SO.ColorYellow, Card_SO.ColorGreen, Card_SO.ColorCyan};
+
         switch (color)
         {
-            case "red": this.GetComponent<MeshRenderer>().material = Card_SO.ColorRed; break;
-            case "green": this.GetComponent<MeshRenderer>().material = Card_SO.ColorGreen; break;
-            case "yellow": this.GetComponent<MeshRenderer>().material = Card_SO.ColorYellow; break;
-            case "cyan": this.GetComponent<MeshRenderer>().material = Card_SO.ColorCyan; break;
+            case "red": this.GetComponent<MeshRenderer>().material = mats[0]; break;
+            case "yellow": this.GetComponent<MeshRenderer>().material = mats[1]; break;
+            case "green": this.GetComponent<MeshRenderer>().material = mats[2]; break;
+            case "cyan": this.GetComponent<MeshRenderer>().material = mats[3]; break;
+            default:  this.GetComponent<MeshRenderer>().material = mats[Random.Range(0, mats.Length)]; break;
         }        
     }
     public void SetValue(string val = "z")
     {
-        if (val != "z")
-        {
-            this.Value = val[0];
-            MiddleText.text = val;
-            TopLeftText.text = val;
-            BottomRightText.text = val;
-        }
-        else
-        {
-            string value = Random.Range(1, 10).ToString();
-            this.Value = value[0];
-            MiddleText.text = value;
-            TopLeftText.text = value;
-            BottomRightText.text = value;
-        }
-        
-        
+        if (val == "z")
+            val = Random.Range(1, 10).ToString();
+
+        this.Value = val[0];
+        MiddleText.text = val;
+        TopLeftText.text = val;
+        BottomRightText.text = val;
     }
 }
